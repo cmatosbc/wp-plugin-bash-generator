bbd9532fa92d26216b2cf6c987756aaf4c5114d3
Carlos MAtos <carlosarturmatos1977@gmail.com>
2025-01-03 03:12:35 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 const path = require('path');
 
-// Standard assets configuration
-const standardConfig = {
+module.exports = {
     entry: {
         'main': './src/js/main.js',
-        'admin': './src/js/admin.js'
+        'admin': './src/js/admin.js',
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'assets/js')
+        path: path.resolve(__dirname, 'assets/js'),
     },
     module: {
         rules: [
@@ -26,16 +24,3 @@ const standardConfig = {
         ]
     }
 };
-
-// Gutenberg blocks configuration
-const gutenbergConfig = {
-    ...defaultConfig,
-    entry: {
-        'blocks/index': path.resolve(__dirname, 'src/blocks/index.js'),
-        'patterns/index': path.resolve(__dirname, 'src/patterns/index.js'),
-        'templates/index': path.resolve(__dirname, 'src/templates/index.js'),
-        'extensions/index': path.resolve(__dirname, 'src/extensions/index.js')
-    }
-};
-
-module.exports = [standardConfig, gutenbergConfig];
